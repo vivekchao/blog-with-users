75f9304c8278d62ed2fb38e4889232e68882053d
vivekchao <chaonesskh@gmail.com>
2021-12-17 17:40:54 +0800

    Updated env var

--- main.py
+++ main.py
@@ -12,16 +12,17 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 # Import RegisterForm from forms.py
 from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
